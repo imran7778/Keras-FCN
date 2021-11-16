210430077c93808a702d4bdc3108133036946bf0
imran7778 <31092568+imran7778@users.noreply.github.com>
2021-11-16 17:16:08 +0100

    sdasc

--- models.py
+++ models.py
@@ -89,3 +89,23 @@ def fcn32(vgg16, l2=0):
     return keras.Model(vgg16.input, x)
 
 
+
+
+class BilinearInitializer(keras.initializers.Initializer):
+    '''Initializer for Conv2DTranspose to perform bilinear interpolation on each channel.'''
+    def __call__(self, shape, dtype=None, **kwargs):
+        kernel_size, _, filters, _ = shape
+        arr = np.zeros((kernel_size, kernel_size, filters, filters))
+        ## make filter that performs bilinear interpolation through Conv2DTranspose
+        upscale_factor = (kernel_size+1)//2
+        if kernel_size % 2 == 1:
+            center = upscale_factor - 1
+        else:
+            center = upscale_factor - 0.5
+        og = np.ogrid[:kernel_size, :kernel_size]
+        kernel = (1-np.abs(og[0]-center)/upscale_factor) * \
+                 (1-np.abs(og[1]-center)/upscale_factor) # kernel shape is (kernel_size, kernel_size)
+        for i in range(filters):
+            arr[..., i, i] = kernel
+        return tf.convert_to_tensor(arr, dtype=dtype)
+
