b8bedf3e6014068199bbad17a0c18510e6babd06
imran7778 <31092568+imran7778@users.noreply.github.com>
2021-11-17 18:00:28 +0100

    scds

--- models.py
+++ models.py
@@ -113,4 +113,27 @@ class BilinearInitializer(keras.initializers.Initializer):
         for i in range(filters):
             arr[..., i, i] = kernel
         return tf.convert_to_tensor(arr, dtype=dtype)
+    
+    
+    
+    class MyMeanIoU(keras.metrics.MeanIoU):
+    '''Custom meanIoU to handle borders (class = -1).'''
+    def update_state(self, y_true, y_pred_onehot, sample_weight=None):
+        y_pred = tf.argmax(y_pred_onehot, axis=-1)
+        ## add 1 so boundary class=0
+        y_true = tf.cast(y_true+1, self._dtype)
+        y_pred = tf.cast(y_pred+1, self._dtype)
+        ## Flatten the input if its rank > 1.
+        if y_pred.shape.ndims > 1:
+            y_pred = tf.reshape(y_pred, [-1])
+        if y_true.shape.ndims > 1:
+            y_true = tf.reshape(y_true, [-1])
+        ## calculate confusion matrix with one extra class
+        current_cm = tf.math.confusion_matrix(
+            y_true,
+            y_pred,
+            self.num_classes+1,
+            weights=sample_weight,
+            dtype=self._dtype)
+        return self.total_cm.assign_add(current_cm[1:, 1:]) # remove boundary
 
